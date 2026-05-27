618b37482c93f0748a40962207cb2ffd0ff38bfd
Adriano-Paz <adriano.agp15@gmail.com>
2026-05-26 22:22:15 -0300

    adicionando as bibliotecas necessária para o projeto
    adicionando as bibliotecas, e criando as subpastas do projeto

---
+++ roteirodovinho/lib/screens/homesreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homesreen extends StatefulWidget {
+  const Homesreen({super.key});
+
+  @override
+  State<Homesreen> createState() => _HomesreenState();
+}
+
+class _HomesreenState extends State<Homesreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- roteirodovinho/macos/Flutter/GeneratedPluginRegistrant.swift
+++ roteirodovinho/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import file_selector_macos
+import geolocator_apple
+import package_info_plus
+import share_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
